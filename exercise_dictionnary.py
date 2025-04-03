e9ec0f1b147404a167bd671d4facf86e3bdc7be0
Sambandha Magar <89954672+Sambandha456@users.noreply.github.com>
2025-04-03 09:54:59 +0000

    Dictionnary

---
+++ exercise_dictionnary.py
@@ -0,0 +1,8 @@
+student ={
+    "first_name": "John",
+    "last_name" : "Doe",
+    "age" : 20,
+}
+print("First Name :",student["first_name"])
+print("Last Name :",student["last_name"])
+print("Age :",student["age"])
