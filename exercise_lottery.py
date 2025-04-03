57bed8d9856f1b1a1dbf183b3b381d28b1d8c9ac
Sambandha Magar <89954672+Sambandha456@users.noreply.github.com>
2025-04-03 09:37:06 +0000

    modified

--- exercise_lottery.py
+++ exercise_lottery.py
@@ -1,15 +1,18 @@
 user_number = set()
 
 while True:
-    user = int(input("Enter the Number: "))
-    if user in user_number:
-        print("Error")
-        break
-    else:
-        user_number.add(user)
-    if len(user_number) >= 7:
-        for num in user_number:
-            print(f"The numbers are {num}")
-        break
+    try :
+        user = int(input("Enter the Number: "))
+        if user in user_number:
+            print("Error")
+            break
+        else:
+            user_number.add(user)
+        if len(user_number) >= 7:
+            for num in user_number:
+                print(f"The numbers are {num}")
+            break
+    except ValueError:
+        print("Enter only the number")
 
         
